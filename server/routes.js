dd7c28f8ce226527a5b80d93d1f02583ef7f0d40
beatrizmakowski <bemakow@gmail.com>
2022-06-08 14:19:22 -0300

    feat(routes): adding routes GET, PUT and DELETE

--- server/routes.js
+++ server/routes.js
@@ -20,11 +20,54 @@ router.post('/', async (req, res) => {
 })
 
 // Get all
+router.get('/', async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo")
+        return res.status(200).json(allTodos.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 // Get one
+router.get('/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+        if (todo.rows[0] == undefined) {
+            return res.status(404).json("Todo not found!")
+        }
+        return res.status(200).json(todo.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 // Update one
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { description } = req.body
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2", 
+            [description, id]
+        )
+        // How to return updated item?
+        return res.status(204).json(`Todo with id ${id} updated successfully.`)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 
 // Delete one
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+        return res.status(204).json(`Todo with id ${id} deleted successfully.`)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 
 module.exports = router
